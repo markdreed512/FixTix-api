abcff488386dc9d6758dbb6784798cf1976ff481
Mark Reed <markdreed512@gmail.com>
2021-06-17 11:39:57 -0500

    signup and login routes with bcrypt

--- api/models.py
+++ api/models.py
@@ -4,9 +4,11 @@ from . import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), unique=True, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     image_file = db.Column(db.String(240))
+    role = db.Column(db.String(240), nullable=False)
     tickets = db.relationship('Ticket', backref='author', lazy=True)
     
     def __repr__(self):
@@ -20,7 +22,7 @@ class Project(db.Model):
 
     def __repr__(self):
         return f"Project( '{self.id}', '{self.title}'"
-        
+
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- api/views.py
+++ api/views.py
@@ -1,8 +1,26 @@
 from flask import Blueprint, jsonify, request
 from . import db
 from .models import Ticket, User, Project
-
+import bcrypt
 main = Blueprint('main', __name__)
+
+@main.route('/login')
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if not username: 
+        return "Missing username", 400
+    if not password:
+        return "Missing password", 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "User not found"
+    if bcrypt.checkpw(password.encode('utf-8'), user.password):
+        return "Welcome back " + username + "!!"
+    else:
+        return "Wrong Password!!"
 
 @main.route('/add_project', methods=['POST'])
 def add_project():
@@ -27,18 +45,29 @@ def add_ticket():
 
 @main.route('/add_user', methods=['POST'])
 def add_user():
-    user_data = request.get_json()
-    new_user = User(username=user_data['username'], password=user_data['password'], image_file=user_data['image_file'])
+    email = request.json.get('email', None)
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    image_file = request.json.get('image_file', None)
+    role = "user"
+
+    if not email:
+        return "Missing email", 400
+    if not username: 
+        return "Missing username", 400
+    if not password:
+        return "Missing password", 400
+
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    new_user = User(email=email, password=hashed_pw, username=username, image_file=image_file, role=role)
 
     db.session.add(new_user)
     db.session.commit()
 
-    return 'Done', 201
-# @app.route('your route', methods=['GET'])
-# def yourMethod(params):
-#     response = flask.jsonify({'some': 'data'})
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+    return 'Welcome, ' + username + '!!'
+    # new_user = User(username=user_data['username'], password=user_data['password'], image_file=user_data['image_file'])
+
 @main.route('/tickets')
 def tickets():
     tickets_list = Ticket.query.all()
